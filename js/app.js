7697f7cd551c600ee2f801490642de56b976b68e
davidhoos <david.c.hoos@gmail.com>
2015-02-26 16:34:29 -0800

    completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -21,87 +20,83 @@ $(document).ready(function(){
 	$("#guessButton").click(function(evt) {
 		numDifference();
 		evt.preventDefault();
+		countUp();
 	});
+
+	autoNumber();
 
 });
 
-//Create a newGame function that resets all settings.
-
-function newGame() {
-	
-	// Create new random number
-	$("#userGuess").val("");
-
-	// Reset number of guesses
-	autoNumber();
-
-
-
-}
 
 
 // Mystery number generator
 function autoNumber(){
 	randomNumber = Math.floor((Math.random() * 100) + 1);
-	console.log("test");
 }
+
 
 // Is the guess hot or cold?
-function numDifference(){
-	
-	var userGuess = $("#userGuess").val();
-	var numDiff = Math.abs(userGuess - randomNumber);
-	if (numDiff == 0) {
-		$("#guessList").append("<li>You are correct!</li>");
-	}
-
-	else if (numDiff < 10) {
-		$("#guessList").append("<li>You are very hot</li>");
-	}
-
-	else if (numDiff < 20) {
-		$("#guessList").append("<li>You are hot</li>");
-	}
-
-	else if (numDiff < 30) {
-		$("#guessList").append("<li>You are warm</li>");
-	}
-
-	else if (numDiff < 51) {
-		$("#guessList").append("<li>You are cold</li>");
-	}
-
-	else {
-		$("#guessList").append("<li>You are very cold</li>");
-	}
-}
-
-//Guess feedback goes to: div#feedback
-
-
-
-
-// The game should track how many guess the user has made. Feedback about this 
-// should appear in span#count (which defaults to 0, when the page loads).
 
 // The game should also supply users with a list of the numbers they have guessed
 // so far. The CSS for this game is set up in such a way that you can simply add 
 // each guessed number as an <li> to ul#guessList
 
+function numDifference(){
+	
+	var userGuess = $("#userGuess").val();
+	var numDiff = Math.abs(userGuess - randomNumber);
+
+	if (numDiff == 0) {
+		$("#guessList").append("<li>You are correct!</li>");
+	}
+
+	else if (numDiff < 10) {
+		$("#guessList").append("<li>" + userGuess + " - You are very hot</li>");
+	}
+
+	else if (numDiff < 20) {
+		$("#guessList").append("<li>" + userGuess + " - You are hot</li>");
+	}
+
+	else if (numDiff < 30) {
+		$("#guessList").append("<li>" + userGuess + " - You are warm</li>");
+	}
+
+	else if (numDiff < 51) {
+		$("#guessList").append("<li>" + userGuess + " - You are cold</li>");
+	}
+
+	else {
+		$("#guessList").append("<li>" + userGuess + " - You are very cold</li>");
+	}
+
+}
+
+//Guess feedback goes to: div#feedback
+
+	// Move the counter back to zero
+function resetCounter() {
+        $('#count').html(0);
+        count = 0;
+}
+
+// The game should track how many guess the user has made. Feedback about this 
+// should appear in span#count (which defaults to 0, when the page loads).
+var  count = 0;
+
+function countUp() {
+	// Add one to the counter each time this function is run
+        count++;
+        $('#count').html(count);
+        console.log;
+}
 
 
-// You’ll need to ensure that users provide valid inputs. Note that the guess text 
-// input field has the HTML 5 required flag set, so you won’t have to worry about 
-// blank guesses being submitted (if the user submits a blank guess, they'll be 
-// prompted to supply an input). However, you will need to write code that ensures 
-// that the user has supplied a numeric input between 1 and 100.
+//Create a newGame function that resets all settings.
 
-
-// The starter template already contains a button in the upper right hand corner for 
-// starting a new game, however, this button does not currently do anything. You’ll 
-// need to write code that allows users to start a new game without making additional 
-// calls to the server. Clicking “New Game” should trigger the JavaScript function 
-// that starts a new game.
+function newGame() {
+	location.reload();
+}
 
 
 
